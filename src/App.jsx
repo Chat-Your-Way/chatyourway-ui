000378a76b9574157b72d066a995240136ab60ce
mykhalenych <mykhalenych@gmail.com>
2023-07-26 11:23:27 +0300

    feature/initEslintConfigs set up eslint, prettier, added readme.md

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+    return <div className="App">hello world</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-    </React.StrictMode>
-    </Provider>
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
 );
-
-
-
