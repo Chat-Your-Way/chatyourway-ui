27c7a091279914a4eeaceb4cfa6100b45cd313ef
Illia Komissarov <iliakomissarow99@gmail.com>
2023-07-31 13:26:08 +0300

    minor changes
    lightTheme added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme/theme";
+import lightTheme from "./theme/theme";
 import Typography from "@mui/material/Typography";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Typography variant="myCustomVariant">
-        hello world!
-      </Typography>
+    <ThemeProvider theme={lightTheme}>
+      <Typography variant="myCustomVariant">hello world!</Typography>
     </ThemeProvider>
   );
 }
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,53 +1,52 @@
 import { createTheme } from "@mui/material/styles";
 
-const createCommonTheme = (theme) =>
-  createTheme({
-    palette: {
-      primary: {
-        white: "#FFFFFF",
-        light: "#ACADFF",
-        main: "#8686DC",
-        dark: "#353535",
-        contrastText: "#6261AF",
-      },
+const lightPalette = createTheme({
+  palette: {
+    primary: {
+      white: "#FFFFFF",
+      light: "#ACADFF",
+      main: "#8686DC",
+      dark: "#353535",
+      contrastText: "#6261AF",
     },
-    typography: {
-      h1: {
-        fontFamily: "Inter",
-        fontSize: "48px",
-        fontWeight: "regular",
-      },
-      h2: {
-        fontFamily: "Inter",
-        fontSize: "40px",
-        fontWeight: "regular",
-      },
-      h3: {
-        fontFamily: "Inter",
-        fontSize: "32px",
-        fontWeight: "regular",
-      },
-      h4: {
-        fontFamily: "Inter",
-        fontSize: "24px",
-        fontWeight: "regular",
-      },
-      h5: {
-        fontFamily: "Inter",
-        fontSize: "16px",
-        fontWeight: "regular",
-      },
-      h6: {
-        fontFamily: "Inter",
-        fontSize: "14px",
-        fontWeight: "regular",
-      },
-      myCustomVariant: {
-        fontSize: "100px",
-        fontWeight: "bold",
-        color: "#FF0000",
-      },
+  },
+  typography: {
+    h1: {
+      fontFamily: "Inter",
+      fontSize: "48px",
+      fontWeight: "regular",
     },
-  });
+    h2: {
+      fontFamily: "Inter",
+      fontSize: "40px",
+      fontWeight: "regular",
+    },
+    h3: {
+      fontFamily: "Inter",
+      fontSize: "32px",
+      fontWeight: "regular",
+    },
+    h4: {
+      fontFamily: "Inter",
+      fontSize: "24px",
+      fontWeight: "regular",
+    },
+    h5: {
+      fontFamily: "Inter",
+      fontSize: "16px",
+      fontWeight: "regular",
+    },
+    h6: {
+      fontFamily: "Inter",
+      fontSize: "14px",
+      fontWeight: "regular",
+    },
+    myCustomVariant: {
+      fontSize: "100px",
+      fontWeight: "bold",
+      color: "#FF0000",
+    },
+  },
+});
 
-export default createCommonTheme;
+export default lightPalette;
