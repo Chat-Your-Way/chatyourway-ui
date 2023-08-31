488380b3aed9ee78d19c8443d429ba03d1e76495
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-08-31 23:47:14 +0300

    fix: add aria label and onChange handler to checkbox

--- src/ui-kit/components/CheckBox/CheckBox.jsx
+++ src/ui-kit/components/CheckBox/CheckBox.jsx
@@ -1,12 +1,23 @@
 import { StyledCheckbox, StyledFormControlLabel } from './CheckBox.styled';
 
-function CheckBox({ label = '', inputColor, labelColor, ...props }) {
+function CheckBox({
+  label = '',
+  handleChange,
+  inputColor,
+  labelColor,
+  ariaLabel = '',
+  ...props
+}) {
   return (
     <StyledFormControlLabel
       control={
         <StyledCheckbox
           inputColor={inputColor}
           labelColor={labelColor}
+          onChange={handleChange}
+          inputProps={{
+            'aria-label': ariaLabel,
+          }}
           {...props}
         />
       }
