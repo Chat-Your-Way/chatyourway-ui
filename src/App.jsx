14ac76fc122bacdfd63b7d28b2e4f85953cba485
miamarichka <ilchyshyn.marichka@gmail.com>
2023-09-02 16:00:02 +0300

    fixes comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material';
 import lightTheme from './ui-kit/theme/theme';
 import './App.css';
-import EmailInput from './ui-kit/components/Input/EmailInput/EmailInput';
-import SearchInput from './ui-kit/components/Input/SearchInput/SearchInput';
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <Typography variant="h1">hello world!</Typography>
-      <EmailInput />
-      <SearchInput />
     </ThemeProvider>
   );
 }
