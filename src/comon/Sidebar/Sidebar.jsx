bdf886d18168ed9b7d014030d883f85a72555469
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-09-20 18:57:44 +0300

    fix: update sidebar, change logic show active link

--- src/comon/Sidebar/Sidebar.jsx
+++ src/comon/Sidebar/Sidebar.jsx
@@ -1,67 +1,51 @@
-import { memo } from 'react';
 import {
   StyledBox,
-  ActiveLink,
-  ActiveText,
-  DefaultLink,
-  DefaultText,
+  StyledNavLink,
+  StyledItem,
+  StyledText,
 } from './Sidebar.styled';
 import { ICONS } from '../../ui-kit/icons';
 import { useSidebarContext } from './SidebarContext';
 
 const Sidebar = () => {
-  const curPath = 'main'; // the path will be obtained from routing
-
   const { showText } = useSidebarContext();
 
   return (
     <StyledBox showText={showText}>
-      {curPath === 'main' ? (
-        <ActiveLink showText={showText}>
-          <ICONS.CHAT />
-          {showText && <ActiveText>Chats</ActiveText>}
-        </ActiveLink>
-      ) : (
-        <DefaultLink showText={showText}>
-          <ICONS.CHAT />
-          {showText && <DefaultText>Chats</DefaultText>}
-        </DefaultLink>
-      )}
-      {curPath === 'notification' ? (
-        <ActiveLink showText={showText}>
-          <ICONS.MESSAGE />
-          {showText && <ActiveText>MESSAGE</ActiveText>}
-        </ActiveLink>
-      ) : (
-        <DefaultLink showText={showText}>
-          <ICONS.MESSAGE />
-          {showText && <DefaultText>Notification</DefaultText>}
-        </DefaultLink>
-      )}
-      {curPath === 'settings' ? (
-        <ActiveLink showText={showText}>
-          <ICONS.SETTING />
-          {showText && <ActiveText>Settings</ActiveText>}
-        </ActiveLink>
-      ) : (
-        <DefaultLink showText={showText}>
-          <ICONS.SETTING />
-          {showText && <DefaultText>Settings</DefaultText>}
-        </DefaultLink>
-      )}
-      {curPath === 'info' ? (
-        <ActiveLink showText={showText}>
-          <ICONS.INFO_SQUARE />
-          {showText && <ActiveText>FAQ</ActiveText>}
-        </ActiveLink>
-      ) : (
-        <DefaultLink showText={showText}>
-          <ICONS.INFO_SQUARE />
-          {showText && <DefaultText>FAQ</DefaultText>}
-        </DefaultLink>
-      )}
+      <StyledNavLink to="/">
+        {({ isActive }) => (
+          <StyledItem showText={showText} isActive={isActive}>
+            <ICONS.CHAT />
+            <StyledText isActive={isActive}>Чати</StyledText>
+          </StyledItem>
+        )}
+      </StyledNavLink>
+      <StyledNavLink to="/notification">
+        {({ isActive }) => (
+          <StyledItem showText={showText} isActive={isActive}>
+            <ICONS.MESSAGE />
+            <StyledText isActive={isActive}>Повідомлення</StyledText>
+          </StyledItem>
+        )}
+      </StyledNavLink>
+      <StyledNavLink to="/settings">
+        {({ isActive }) => (
+          <StyledItem showText={showText} isActive={isActive}>
+            <ICONS.SETTING />
+            <StyledText isActive={isActive}>Налаштування</StyledText>
+          </StyledItem>
+        )}
+      </StyledNavLink>
+      <StyledNavLink to="/info">
+        {({ isActive }) => (
+          <StyledItem showText={showText} isActive={isActive}>
+            <ICONS.INFO_SQUARE />
+            <StyledText isActive={isActive}>FAQ</StyledText>
+          </StyledItem>
+        )}
+      </StyledNavLink>
     </StyledBox>
   );
 };
 
-export default memo(Sidebar);
+export default Sidebar;
--- src/comon/Sidebar/Sidebar.styled.js
+++ src/comon/Sidebar/Sidebar.styled.js
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
 export const StyledBox = styled(Box, {
   shouldForwardProp: (p) => p !== 'showText',
@@ -11,22 +11,25 @@ export const StyledBox = styled(Box, {
   box-sizing: border-box;
 `;
 
-export const ActiveLink = styled(Link, {
+export const StyledItem = styled(Box, {
   shouldForwardProp: (p) => p !== 'showText',
 })`
+  ${(p) => p.theme.typography.h5}
   padding: 16px;
-  width: 290px;
+  cursor: ${(p) => (p.isActive ? 'default' : 'pointer')};
+  width: ${(p) => (p.showText ? '290px' : '75px')};
   display: flex;
   align-items: center;
   text-decoration: none;
-  gap: 8px;
+  gap: ${(p) => (p.showText ? '8px' : '0')};
   box-sizing: border-box;
-  font-size: ${(p) => p.theme.typography.h5.fontSize};
   border-radius: 16px;
   border: 1px solid
-    ${(p) => (p.showText ? p.theme.palette.primary.main : 'transparent')};
+    ${(p) =>
+      p.showText && p.isActive ? p.theme.palette.primary.main : 'transparent'};
   color: ${(p) => p.theme.palette.primary.main};
-  background: ${(p) => p.theme.palette.primary.white};
+  background: ${(p) =>
+    p.showText && p.isActive ? p.theme.palette.primary.white : 'transparent'};
   transition: 0.2s ease all;
 
   svg {
@@ -35,36 +38,13 @@ export const ActiveLink = styled(Link, {
   }
 
   path {
-    fill: ${(p) => p.theme.palette.primary.main};
-  }
-`;
-
-export const DefaultLink = styled(Link, {
-  shouldForwardProp: (p) => p !== 'showText',
-})`
-  padding: 16px;
-  width: 290px;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  gap: 8px;
-  box-sizing: border-box;
-  font-size: ${(p) => p.theme.typography.h5.fontSize};
-  border-radius: 16px;
-  border: 1px solid transparent;
-  color: ${(p) => p.theme.palette.primary.dark};
-  background: transparent;
-  cursor: pointer;
-  transition: 0.2s ease all;
-
-  svg {
-    width: ${(p) => (p.showText ? '35px' : '75px')};
-    height: ${(p) => (p.showText ? '35px' : '75px')};
+    fill: ${(p) => (p.isActive ? p.theme.palette.primary.main : undefined)};
   }
 
   &:hover {
     border: 1px solid
       ${(p) => (p.showText ? p.theme.palette.primary.main : 'transparent')};
+
     color: ${(p) => p.theme.palette.primary.main};
 
     svg {
@@ -73,19 +53,21 @@ export const DefaultLink = styled(Link, {
     }
 
     path {
-      fill: ${(p) => (p.showText ? p.theme.palette.primary.main : '')};
+      fill: ${(p) => (p.showText ? p.theme.palette.primary.main : undefined)};
     }
   }
 `;
 
-export const DefaultText = styled(Box)`
-  color: ${(p) => p.theme.palette.primary.dark};
+export const StyledText = styled(Box)`
+  color: ${(p) =>
+    p.isActive ? p.theme.palette.primary.main : p.theme.palette.primary.dark};
 
   &:hover {
-    color: ${(p) => p.theme.palette.primary.main};
+    color: ${(p) =>
+      p.isActive ? 'transparent' : p.theme.palette.primary.main};
   }
 `;
 
-export const ActiveText = styled(Box)`
-  color: ${(p) => p.theme.palette.primary.main};
+export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
 `;
