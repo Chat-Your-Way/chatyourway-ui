89a4c68b970b00e646b8fda998a1afe930dedc91
khardy <khardy.official@gmail.com>
2023-09-20 20:38:23 +0300

    added FaqPageWrapper & FaqPageTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-/* eslint-disable max-len */
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './ui-kit/components/SharedLayout/SharedLayout';
 import { CombinedThemeProvider } from './ui-kit/theme/ThemeProvider';
-import { SidebarContextProvider } from './common/Sidebar/SidebarContext';
-import { TopicsContextProvider } from './common/Topics/TopicsContext';
-import { TopicsPageContextProvider } from './pages/TopicsPage/TopicsPageContext';
-import Router from './components/Routes/Route';
+import FaqPage from './pages/FaqPage';
 
 function App() {
   return (
     <CombinedThemeProvider>
-      <SidebarContextProvider>
-        <TopicsContextProvider>
-          <TopicsPageContextProvider>
-            <Router />
-          </TopicsPageContextProvider>
-        </TopicsContextProvider>
-      </SidebarContextProvider>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index />
+          <Route path="/faq" element={<FaqPage />} />
+        </Route>
+      </Routes>
     </CombinedThemeProvider>
   );
 }
