4c39aad4ad9b687489a96123a2b135269d327ce8
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-09-21 14:49:55 +0300

    feat: add routes obj

---
+++ src/constans/routes.js
@@ -0,0 +1,22 @@
+export const routes = {
+  main: {
+    path: '/',
+    name: 'Чати',
+    icon: 'CHAT',
+  },
+  notification: {
+    path: '/notification',
+    name: 'Повідомлення',
+    icon: 'MESSAGE',
+  },
+  settings: {
+    path: '/settings',
+    name: 'Налаштування',
+    icon: 'SETTING',
+  },
+  info: {
+    path: '/info',
+    name: 'FAQ',
+    icon: 'INFO_SQUARE',
+  },
+};
