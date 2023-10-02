bb495f21afb7fd32baa610be59fe9d31c1d75760
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-10-02 17:43:31 +0300

    fix: fix component layout

--- src/ui-kit/components/SharedLayout/SharedLayout.jsx
+++ src/ui-kit/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import MainBackground from '../MainBackground';
 import Header from '../../../components/Layouts/Header';
 import Footer from '../../../components/Layouts/Footer';
-import Topics from '../../../common/Topics/Topics';
 
 const SharedLayout = () => {
   return (
@@ -10,7 +9,6 @@ const SharedLayout = () => {
       <Header />
       <main>
         <Outlet />
-        <Topics />
       </main>
       <Footer />
     </MainBackground>
