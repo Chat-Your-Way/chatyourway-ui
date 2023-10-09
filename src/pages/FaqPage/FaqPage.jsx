32a780aee5a94eb39720bae4f2b59e7deb49f323
khardy <khardy.official@gmail.com>
2023-10-09 23:30:34 +0300

    feat: CHAT-82/CHAT-99 adaptive FAQ page

--- src/pages/FaqPage/FaqPage.jsx
+++ src/pages/FaqPage/FaqPage.jsx
@@ -1,9 +1,44 @@
-import { FaqPageWrapper, FaqPageTitle } from './FaqPage.styled';
+import {
+  FaqPageTitle,
+  FaqPageWrapper,
+  FaqAccordion,
+  IconDown,
+  FaqAccordionSummary,
+  FaqAccordionDetails,
+} from './FaqPage.styled';
+
+const titlesAndTexts = [
+  {
+    title: 'How to block a message',
+    text: 'How to block a message - asdfsdafagfasg',
+  },
+  {
+    title: 'How to ban a user',
+    text: 'How to ban a user - ffffffffffffff',
+  },
+  {
+    title: 'How to complain to the chat',
+    text: 'How to complain to the chat - fatsADTASDTATSDTASD',
+  },
+  {
+    title:
+      'Lorem ipsum dolor sii in ex eleifend, faucibus libero ac, lobortis leo.',
+    text: 'Lorem ipsum dolor sit amet, conseckskdfksdf - sasdfsdsdfdsf',
+  },
+];
 
 const FaqPage = () => {
   return (
     <FaqPageWrapper>
       <FaqPageTitle variant="h1">The most common questions</FaqPageTitle>
+      {titlesAndTexts.map((item, index) => (
+        <FaqAccordion key={index}>
+          <FaqAccordionSummary expandIcon={<IconDown />}>
+            {item.title}
+          </FaqAccordionSummary>
+          <FaqAccordionDetails>{item.text}</FaqAccordionDetails>
+        </FaqAccordion>
+      ))}
     </FaqPageWrapper>
   );
 };
