353c2f77448736f1303cb5bef79e3437da89c478
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-10-19 15:01:47 +0300

    fix: add API_BASE_URL to .env

--- src/redux/apiParams.js
+++ src/redux/apiParams.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://chat.eu-central-1.elasticbeanstalk.com';
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export const Referer = document.referrer;
 export const ajwt = () => JSON.parse(localStorage.getItem('accessToken'));
 export const rjwt = () => JSON.parse(localStorage.getItem('refreshToken'));
