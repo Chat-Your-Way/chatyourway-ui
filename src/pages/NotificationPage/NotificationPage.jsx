fd2891f86521f70a4a79fe3b037d6958ad3af79e
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-11-02 21:17:38 +0200

    fix NotificationPage

--- src/pages/NotificationPage/NotificationPage.jsx
+++ src/pages/NotificationPage/NotificationPage.jsx
@@ -1,15 +1,11 @@
-import { Outlet, useLocation } from 'react-router';
+import { Outlet } from 'react-router';
 import { NotificationPageWrap } from './NotificationPage.styled';
 import Topics from '../../common/Topics';
-import Chat from '../../components/Chat';
 
 const NotificationPage = () => {
-  const { pathname } = useLocation();
-
   return (
     <NotificationPageWrap>
       <Topics isTopics={false} />
-      {pathname === '/notification/chat' && <Chat />}
       <Outlet />
     </NotificationPageWrap>
   );
