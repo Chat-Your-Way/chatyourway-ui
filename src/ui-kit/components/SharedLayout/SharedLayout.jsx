0947032ee291731b8beb987e33a8f91aee8be9ee
Vladyslav <dyadyavadya308@gmail.com>
2023-11-06 20:30:04 +0200

    deleted test

--- src/ui-kit/components/SharedLayout/SharedLayout.jsx
+++ src/ui-kit/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import MainBackground from '../MainBackground';
 import Header from '../../../components/Layouts/Header';
 import Footer from '../../../components/Layouts/Footer';
-import TextNavLinkButton from '../TextNavLinkButton/TextNavLinkButton';
 
 const SharedLayout = () => {
   return (
@@ -10,7 +9,6 @@ const SharedLayout = () => {
       <Header />
       <main>
         <Outlet />
-        <TextNavLinkButton to={'/'} label={'To main'} />
       </main>
       <Footer />
     </MainBackground>
