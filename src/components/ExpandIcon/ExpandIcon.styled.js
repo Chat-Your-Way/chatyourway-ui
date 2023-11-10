54cb7f0773115ca6b5ad52aa0584ad9a2b182493
Vladyslav <dyadyavadya308@gmail.com>
2023-11-10 17:03:43 +0200

    replced styles in CLOSE/OPEN icons

--- src/components/ExpandIcon/ExpandIcon.styled.js
+++ src/components/ExpandIcon/ExpandIcon.styled.js
@@ -1,13 +1,20 @@
 import styled from '@emotion/styled';
 import { ICONS } from '../../ui-kit/icons';
+import { Box } from '@mui/material';
 
-export const OpenIcon = styled(ICONS.ARROW_DOWN2)`
-  path {
-    fill: ${(p) => p.theme.palette.primary.main};
+export const IconWrapper = styled(Box)`
+  .expandIconWrapper {
+    display: none;
   }
-`;
 
-export const CloseIcon = styled(ICONS.CLOSE_SQUARE)`
+  .Mui-expanded & > .collapsIconWrapper {
+    display: none;
+  }
+
+  .Mui-expanded & > .expandIconWrapper {
+    display: block;
+  }
+
   path {
     fill: ${(p) => p.theme.palette.primary.main};
   }
@@ -16,3 +23,7 @@ export const CloseIcon = styled(ICONS.CLOSE_SQUARE)`
     stroke: ${(p) => p.theme.palette.primary.main};
   }
 `;
+
+export const OpenIcon = styled(ICONS.ARROW_DOWN2)``;
+
+export const CloseIcon = styled(ICONS.CLOSE_SQUARE)``;
