d9cedf3cf14b9524de91f5b20bf389a4e846c1bb
Vladyslav <dyadyavadya308@gmail.com>
2023-11-10 17:17:00 +0200

    replaced FAQpage to component

---
+++ src/components/FaqPageComponent/FaqPageComponent.jsx
@@ -0,0 +1,47 @@
+import {
+  FaqPageTitle,
+  FaqPageWrapper,
+  FaqAccordion,
+  FaqAccordionDetails,
+} from './FaqPageComponent.styled';
+import AccordionSummary from '@mui/material/AccordionSummary';
+
+import ExpandIcon from '../../components/ExpandIcon';
+
+const titlesAndTexts = [
+  {
+    title: 'How to block a message',
+    text: 'How to block a message - asdfsdafagfasg',
+  },
+  {
+    title: 'How to ban a user',
+    text: 'How to ban a user - ffffffffffffff',
+  },
+  {
+    title: 'How to complain to the chat',
+    text: 'sad very sad',
+  },
+  {
+    title:
+      'Lorem ipsum dolor sii in ex eleifend, faucibus libero ac, lobortis leo.',
+    text: 'Lorem ipsum dolor sit amet, conseckskdfksdf - sasdfsdsdfdsf',
+  },
+];
+
+function FaqPageComponent() {
+  return (
+    <FaqPageWrapper>
+      <FaqPageTitle variant="h2">The most common questions</FaqPageTitle>
+      {titlesAndTexts.map(({ title, text }, index) => (
+        <FaqAccordion key={index}>
+          <AccordionSummary expandIcon={<ExpandIcon />}>
+            {title}
+          </AccordionSummary>
+          <FaqAccordionDetails>{text}</FaqAccordionDetails>
+        </FaqAccordion>
+      ))}
+    </FaqPageWrapper>
+  );
+}
+
+export default FaqPageComponent;
