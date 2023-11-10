a41bf5a4bbd3c74a8c95d878acc34ca0f491c15d
Vladyslav <dyadyavadya308@gmail.com>
2023-11-10 20:34:21 +0200

    one route in APP.JSX deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './ui-kit/components/SharedLayout/SharedLayout';
 import { CombinedThemeProvider } from './ui-kit/theme/ThemeProvider';
-import FaqPage from './pages/FaqPage';
+import { SidebarContextProvider } from './common/Sidebar/SidebarContext';
 
 function App() {
   return (
     <CombinedThemeProvider>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index />
-          <Route path="/faq" element={<FaqPage />} />
-        </Route>
-      </Routes>
+      <SidebarContextProvider>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index />
+          </Route>
+        </Routes>
+      </SidebarContextProvider>
     </CombinedThemeProvider>
   );
 }
