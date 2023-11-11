3b8dfb6a1067f0ba5caff6bf286dbc6143d5aa3a
Vladyslav <dyadyavadya308@gmail.com>
2023-11-11 20:15:24 +0200

    media querries created

---
+++ src/utils/mediaQuerries.js
@@ -0,0 +1,7 @@
+const mediaQueries = {
+  mobile: '@media (min-width: 390px)',
+  tablet: '@media (min-width: 834px)',
+  desktop: '@media (min-width: 1440pxpx)',
+};
+
+export default mediaQueries;
