154cd679854d955c406e765198f24fd165f7e1dc
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-14 20:25:08 +0200

    deleted in sharedlayout

--- src/ui-kit/components/SharedLayout/SharedLayout.jsx
+++ src/ui-kit/components/SharedLayout/SharedLayout.jsx
@@ -2,8 +2,6 @@ import { Outlet } from 'react-router-dom';
 import MainBackground from '../MainBackground';
 import Header from '../../../components/Layouts/Header';
 import Footer from '../../../components/Layouts/Footer';
-/* eslint-disable */
-import VerificationEmail from '../../../components/VerificationEmailPageComponent'
 
 const SharedLayout = () => {
   return (
@@ -11,7 +9,6 @@ const SharedLayout = () => {
       <Header />
       <main>
         <Outlet />
-        <VerificationEmail/>
       </main>
       <Footer />
     </MainBackground>
