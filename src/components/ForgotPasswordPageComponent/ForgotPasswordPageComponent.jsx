f87c981e4c7585005b87e1b52c89e5750212420b
Juliya Boiko <boikojuliya2707@gmail.com>
2023-11-15 14:22:15 +0200

    add ForgotPasswordPage Component adaptive

--- src/components/ForgotPasswordPageComponent/ForgotPasswordPageComponent.jsx
+++ src/components/ForgotPasswordPageComponent/ForgotPasswordPageComponent.jsx
@@ -1,35 +1,79 @@
+import WhiteLayout from '../../ui-kit/components/WhiteLayout/WhiteLayout';
+import { PATH } from '../../constans/routes.js';
+import { useForm } from 'react-hook-form';
+import { useMediaQuery } from 'react-responsive';
+// eslint-disable-next-line max-len
+import { FieldText } from '../RegistrationPageComponent/FieldText/FieldText.jsx';
+import { LoginButton } from '../LoginPageComponent/LoginPageComponent.styled';
 import {
-  ButtonWrapper,
+  LinkIcon,
   CloseIcon,
-  EmailInputStyled,
-  ForgotPasswordText,
-  ForgotPasswordTextWrapper,
-  ForgotPasswordTitle,
+  LogoIcon,
   ForgotPasswordWrapper,
-  SendPasswordButton,
-  WhiteBox,
+  ForgotPasswordTitle,
+  ForgotPasswordText,
+  ForgotPasswordForm,
+  ButtonWrapper,
 } from './ForgotPasswordPageComponent.styled.js';
 
 function ForgotPasswordPageComponent() {
+  const {
+    formState: { errors, isValid },
+    handleSubmit,
+    control,
+  } = useForm({ defaultValues: { email: '' }, mode: 'onChange' });
+  const isTablet = useMediaQuery({ query: '(min-width: calc(845px - 0.02px)' });
+  const isDesktop = useMediaQuery({
+    query: '(min-width: calc(1200px - 0.02px)',
+  });
+
+  const onSubmit = (values) => {
+    const email = values.email.trim().toLowerCase();
+    // eslint-disable-next-line no-console
+    console.log(email);
+  };
+
+  const getWrapperSize = () => {
+    if (isDesktop) {
+      return { width: '800px', height: '600px' };
+    }
+    if (isTablet) {
+      return { width: '624px', height: '800px' };
+    }
+    return { width: '342px', height: '600px' };
+  };
+
   return (
-    <WhiteBox padding="176px 200px">
-      <CloseIcon />
+    <WhiteLayout
+      width={getWrapperSize().width}
+      height={getWrapperSize().height}
+      padding={isTablet ? '40px 112px' : '40px 20px'}
+    >
       <ForgotPasswordWrapper>
-        <ForgotPasswordTextWrapper>
-          <ForgotPasswordTitle variant="h2">
-            Забули свій пароль?
-          </ForgotPasswordTitle>
-          <ForgotPasswordText variant="h6">
-            Введіть, будь ласка, адресу своєї електронної пошти, і ми надішлемо
-            Вам інструкції з відновлення пароля.
-          </ForgotPasswordText>
-        </ForgotPasswordTextWrapper>
-        <EmailInputStyled />
-        <ButtonWrapper>
-          <SendPasswordButton label="Надіслати" />
-        </ButtonWrapper>
+        <LinkIcon to={PATH.LOGIN}>
+          <CloseIcon />
+        </LinkIcon>
+        <LogoIcon />
+        <ForgotPasswordTitle variant="h2">
+          Забули свій пароль?
+        </ForgotPasswordTitle>
+        <ForgotPasswordText variant="h6">
+          Введіть, будь ласка, адресу своєї електронної пошти, і ми надішлемо
+          Вам інструкції з відновлення пароля.
+        </ForgotPasswordText>
+        <ForgotPasswordForm onSubmit={handleSubmit(onSubmit)}>
+          <FieldText
+            title="Email"
+            id="email"
+            control={control}
+            errors={errors.email}
+          />
+          <ButtonWrapper>
+            <LoginButton type="submit" label="Надіслати" disabled={!isValid} />
+          </ButtonWrapper>
+        </ForgotPasswordForm>
       </ForgotPasswordWrapper>
-    </WhiteBox>
+    </WhiteLayout>
   );
 }
 
--- src/components/ForgotPasswordPageComponent/ForgotPasswordPageComponent.styled.js
+++ src/components/ForgotPasswordPageComponent/ForgotPasswordPageComponent.styled.js
@@ -1,50 +1,70 @@
 import styled from '@emotion/styled';
-import { Box, Typography } from '@mui/material';
-import DefaultButton from '../../ui-kit/components/Button';
-import EmailInput from '../../ui-kit/components/Input/EmailInput';
-import WhiteLayout from '../../ui-kit/components/WhiteLayout/WhiteLayout';
 import { ICONS } from '../../ui-kit/icons';
+import { Link } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
 
-export const WhiteBox = styled(WhiteLayout)`
-  position: relative;
+export const LinkIcon = styled(Link)`
+  display: none;
+  @media screen and (min-width: calc(1200px - 0.02px)) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 24px;
+    top: 24px;
+  }
 `;
 
 export const CloseIcon = styled(ICONS.CLOSE_SQUARE)`
-  position: absolute;
-  right: 26px;
-  top: 26px;
+  transition: all 250ms ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 export const ForgotPasswordWrapper = styled(Box)`
-  width: 400px;
+  width: 100%;
   display: flex;
   flex-direction: column;
+  @media screen and (min-width: calc(845px - 0.02px)) {
+    max-width: 400px;
+  }
+`;
+
+export const LogoIcon = styled(ICONS.LOGO)`
+  margin: 0 auto 40px auto;
+  fill: ${(p) =>
+    p.theme.palette.mode === 'light'
+      ? p.theme.palette.primary.contrastText
+      : p.theme.palette.primary.dark};
+  @media screen and (min-width: calc(845px - 0.02px)) {
+    display: none;
+  }
 `;
 
 export const ForgotPasswordTitle = styled(Typography)`
-  text-align: center;
+  text-align: start;
+  font-size: 24px;
   color: ${(p) => p.theme.palette.primary.dark};
+  @media screen and (min-width: calc(845px - 0.02px)) {
+    font-size: 40px;
+  }
 `;
 
 export const ForgotPasswordText = styled(Typography)`
+  margin: 0 0 24px 0;
+  font-size: 14px;
   color: ${(p) => p.theme.palette.primary.dark};
   opacity: 0.6;
 `;
 
-export const ForgotPasswordTextWrapper = styled(Box)`
-  margin-bottom: 24px;
-`;
-
-export const EmailInputStyled = styled(EmailInput)`
-  margin-bottom: 24px;
-`;
-
-export const SendPasswordButton = styled(DefaultButton)`
-  height: 40px;
-  width: 180px;
-  padding: 8px 12px;
+export const ForgotPasswordForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const ButtonWrapper = styled(Box)`
+  margin: 24px 0 0 0;
   text-align: center;
 `;
