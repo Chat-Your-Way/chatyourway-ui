cd18d96bf1138342f0653d396a7adbda5a0209c8
Juliya Boiko <boikojuliya2707@gmail.com>
2023-11-18 11:33:18 +0200

    clear SharedLayout

--- src/ui-kit/components/SharedLayout/SharedLayout.jsx
+++ src/ui-kit/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,10 @@
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import MainBackground from '../MainBackground';
 import Header from '../../../components/Layouts/Header';
 import Footer from '../../../components/Layouts/Footer';
 import Sidebar from '../../../common/Sidebar';
 import { useUser } from '../../../hooks/useUser';
 import { MainWrapper } from './SharedLayout.styled';
-import NotFoundPageComponent from '../../../components/NotFoundPageComponent/NotFoundPageComponent';
 
 const SharedLayout = () => {
   const { isAuthenticated } = useUser();
@@ -16,8 +15,7 @@ const SharedLayout = () => {
       <main>
         <MainWrapper>
           {isAuthenticated && <Sidebar />}
-          {/* <Outlet /> */}
-          <NotFoundPageComponent />
+          <Outlet />
         </MainWrapper>
       </main>
       <Footer />
