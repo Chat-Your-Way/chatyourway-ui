f201e687abcadd8843a4e579ff07701ee0200ef9
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-11-23 14:26:37 +0200

    fix: margin corrected

--- src/components/FaqPageComponent/FaqPageComponent.styled.js
+++ src/components/FaqPageComponent/FaqPageComponent.styled.js
@@ -8,19 +8,22 @@ import {
 } from '@mui/material';
 
 export const FaqPageWrapper = styled(Box)`
-  max-width: 280px;
-  padding: 4px 5px;
+  max-width: 342px;
+  height: 600px;
+  padding: 0 6px;
   margin: 0 auto;
   border-radius: 16px;
-  background-color: ${(p) => p.theme.palette.primary.whiteRBGA};
+  background-color: ${(p) => p.theme.palette.primary.white};
 
   @media screen and (min-width: calc(845px - 0.02px)) {
     padding: 40px 20px;
-    max-width: 400px;
+    max-width: 648px;
+    height: 840px;
   }
 
   @media screen and (min-width: calc(1195px - 0.02px)) {
-    max-width: 600px;
+    max-width: 840px;
+    height: 760px;
     padding: 40px;
   }
 `;
@@ -39,6 +42,7 @@ export const AccordionList = styled(Box)`
   display: flex;
   flex-direction: column;
   gap: 16px;
+  margin: 0;
 `;
 
 export const FaqAccordion = styled(Accordion)`
@@ -47,7 +51,7 @@ export const FaqAccordion = styled(Accordion)`
   }
   box-shadow: none;
   ${(p) => p.theme.typography.h5};
-  border: 1px solid ${(p) => p.theme.palette.primary.light};
+  border: 1px solid ${(p) => p.theme.palette.primary.contrastText};
   border-radius: 16px !important;
   color: ${(p) => p.theme.palette.primary.dark};
   background: ${(p) => p.theme.palette.primary.darkPurple};
@@ -55,12 +59,12 @@ export const FaqAccordion = styled(Accordion)`
   padding: 10px 16px;
 
   .MuiAccordionSummary-content {
-    margin: 0;
+    margin: 0 !important;
     height: auto;
   }
 
   .MuiButtonBase-root {
-    min-height: 0;
+    min-height: 0 !important;
     padding: 0;
   }
 
@@ -69,20 +73,32 @@ export const FaqAccordion = styled(Accordion)`
     padding: 10px 13px;
   }
 
-  & .Mui-expanded {
-    color: ${(p) => p.theme.palette.primary.lightPurple};
+  &.Mui-expanded {
+    color: ${(p) => p.theme.palette.primary.contrastText};
     font-weight: 600;
-    margin: 0;
+    margin: 0 !important;
   }
 `;
+
 export const FaqAccordionSummary = styled(AccordionSummary)`
   line-height: 0;
   min-height: 0;
   margin: 0;
 `;
 
+export const FaqAccordionTitle = styled(Typography)`
+  color: ${(p) => p.theme.palette.primary.dark};
+  .Mui-expanded & {
+    color: ${(p) => p.theme.palette.primary.contrastText};
+    font-weight: 700;
+  }
+`;
+
 export const FaqAccordionDetails = styled(AccordionDetails)`
   margin: 0;
   padding: 0;
+`;
+
+export const FaqAccordionText = styled(Typography)`
   color: ${(p) => p.theme.palette.primary.dark};
 `;
