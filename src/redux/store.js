2514488eb23a4e4c30dd771175b3ab59a2f75e6e
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-12-06 13:23:04 +0200

    feat: add userApi

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationApi from './auth-operations';
 import topicsApi from './topics-operations';
+import userApi from './user-operations';
 
 const store = configureStore({
   reducer: {
     [authenticationApi.reducerPath]: authenticationApi.reducer,
     [topicsApi.reducerPath]: topicsApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(authenticationApi.middleware)
-      .concat(topicsApi.middleware),
+      .concat(topicsApi.middleware)
+      .concat(userApi.middleware),
 });
 
 export default store;
