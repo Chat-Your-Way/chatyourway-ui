260551c7c62ca50e0b3dfe83ca51edf2862de4b5
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-12-07 13:54:46 +0200

    add func permission private message

--- src/components/SettingsPageComponent/UserSettings/PermissionPrivateMessage.js
+++ src/components/SettingsPageComponent/UserSettings/PermissionPrivateMessage.js
@@ -1,12 +1,32 @@
 import { useEffect, useState } from 'react';
+import {
+  usePermitPrivateMessageMutation,
+  useProhibitPrivateMessageMutation,
+} from '../../../redux/user-operations';
 
 export const PermissionPrivateMessage = () => {
   const [isPermission, setIsPermission] = useState(
     localStorage.getItem('permission') || 'true',
   );
 
+  const [permitPrivateMessage] = usePermitPrivateMessageMutation();
+  const [prohibitPrivateMessage] = useProhibitPrivateMessageMutation();
+
+  const togglePermissionRequest = async () => {
+    try {
+      if (isPermission === 'false') {
+        await permitPrivateMessage();
+      } else {
+        await prohibitPrivateMessage();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const togglePermission = () => {
     setIsPermission((prevState) => (prevState === 'true' ? 'false' : 'true'));
+    togglePermissionRequest();
   };
 
   useEffect(() => {
