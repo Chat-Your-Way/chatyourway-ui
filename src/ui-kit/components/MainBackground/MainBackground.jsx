ad19e9346adcd3a500d7152ea1cb2ef51e7d1bba
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-05-28 22:35:47 +0300

    Pulled actual changes of "fix styles" commit for different screens. And added structure of new loader - added icons, fonts, routes.

--- src/ui-kit/components/MainBackground/MainBackground.jsx
+++ src/ui-kit/components/MainBackground/MainBackground.jsx
@@ -1,7 +1,20 @@
 import { SharedLayoutWrapper } from './MainBackground.styled';
 
-const MainBackground = ({ children }) => {
-  return <SharedLayoutWrapper>{children}</SharedLayoutWrapper>;
+const MainBackground = ({
+  children,
+  $direction,
+  $justifyContent,
+  $alignItems,
+}) => {
+  return (
+    <SharedLayoutWrapper
+      $direction={$direction}
+      $justifyContent={$justifyContent}
+      $alignItems={$alignItems}
+    >
+      {children}
+    </SharedLayoutWrapper>
+  );
 };
 
 export default MainBackground;
