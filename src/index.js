378d3f29c456afecac2465e22ccff424251faea1
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-08-18 20:55:17 +0300

    StrictMode turn on

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -11,11 +11,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/">
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+    ,
+  </React.StrictMode>,
 );
