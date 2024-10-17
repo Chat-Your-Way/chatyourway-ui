c6d3e36f78da9a6a6a332491582cfe4c0a866205
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-10-17 22:15:26 +0300

    Add the IconActivityComponent...

--- src/utils/setUnreadMessagesFlag.js
+++ src/utils/setUnreadMessagesFlag.js
@@ -1,18 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-const setUnreadMessageFlag = ({ arrayOfMessages, unreadMessageCount }) => {
-  let tempValue = unreadMessageCount;
+const setUnreadMessageFlag = ({ arrayOfMessages, unreadMessages }) => {
+  // let tempValue = unreadMessageCount;
 
-  if (tempValue === 0) {
+  // if (tempValue === 0) {
+  //   return arrayOfMessages;
+  // } else {
+  //   const result = arrayOfMessages.reduce((acuum, el) => {
+  //     if (tempValue !== 0) {
+  //       tempValue -= 1;
+  //       return [...acuum, { ...el, messageStatus: 'unread' }];
+  //     } else return [...acuum, el];
+  //   }, []);
+  //   return result;
+  // }
+  if (unreadMessages.length === 0) {
     return arrayOfMessages;
-  } else {
-    const result = arrayOfMessages.reduce((acuum, el) => {
-      if (tempValue !== 0) {
-        tempValue -= 1;
-        return [...acuum, { ...el, messageStatus: 'unread' }];
-      } else return [...acuum, el];
-    }, []);
-    return result;
   }
+
+  const result = arrayOfMessages.map((el) => {
+    if (unreadMessages.find((unreadEl) => unreadEl.id === el.id)) {
+      return { ...el, messageStatus: 'unread' };
+    } else return el;
+  });
+
+  return result;
 };
 
 export default setUnreadMessageFlag;
